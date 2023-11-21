2cf2e5538105ad8c631d2781fd0edf160b5703ab
Aarón <aaronmanceracantador2003@gmail.com>
2023-11-21 18:28:48 +0100

    ArmaRayoFinal

--- DuckGame2/Assets/Scripts/ArmaRayo.cs
+++ DuckGame2/Assets/Scripts/ArmaRayo.cs
@@ -8,11 +8,10 @@ public class ArmaRayo : Objeto
 {
     [SerializeField] private LineRenderer lineaDeDisparo;
     private LineRenderer lineaDeDisparoActual;
-    [SerializeField] private float limpiezaSpeed;
+    [SerializeField] private float velocidadDeLimpiezaAsignada;
     private List<Vector3> puntosDelTrail = new List<Vector3>();
     private bool pulsarFire1;
     private bool mantenerFire1;
-
 
     // Start is called before the first frame update
     void Start()
@@ -29,27 +28,71 @@ public class ArmaRayo : Objeto
         pulsarFire1 = Input.GetButtonDown("Fire1");
         mantenerFire1 = Input.GetButton("Fire1");
     }
+    // Update is called in 0.2 seg
     private void FixedUpdate()
     {
+
         Disparar();
-    }
-    private void Disparar()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            DestroyTrailActual();
-            CrearTrailActual();
-            AddPunto();
-        }
-        if (Input.GetButton("Fire1"))
-        {
-            AddPunto();
-        }
-        UpdatePuntosTrail();
-        LimpiarPuntosTrail();
+
     }
     /// <summary>
-    /// Se llamara para crear una nueva linea
+    /// Metodo que nos servira para recoger el arma y activarlo
+    /// </summary>
+    public void Inicializador()
+    {
+        numUsos = 50;
+        gameObject.SetActive(true);
+    }
+    /// <summary>
+    /// Metodo que nos indicara si estamos sin municion y tenemos en cuenta si existe aun una linea de rayo
+    /// </summary>
+    /// <returns></returns>
+    private bool SinMunicion()
+    {
+        Debug.Log(numUsos);
+        if (numUsos <= 0)
+        {
+            if (puntosDelTrail.Count <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+            return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// Este metodo realizara la accion de disparar, siempre y cuando tengamos numero de usos restantes.
+    /// Cuando se nos agote3 el numero de usuos, no se deshabilitara el arma hasta que se acabe la linea actual, y no podremos incrementarla
+    /// </summary>
+    private void Disparar()
+    {
+        if (!SinMunicion())
+        {
+            if (pulsarFire1)
+            {
+                DestroyTrailActual();
+                CrearTrailActual();
+                AddPunto();
+
+            }
+            if (mantenerFire1)
+            {
+                AddPunto();
+            }
+            else
+            {
+                LimpiarPuntosTrail();
+            }
+        }
+        else
+        {
+            LimpiarPuntosTrail();
+        }
+        UpdatePuntosTrail();
+
+    }
+    /// <summary>
+    /// Se llamara para crear una nueva linea de disparo
     /// </summary>
     private void CrearTrailActual()
     {
@@ -58,7 +101,7 @@ public class ArmaRayo : Objeto
         lineaDeDisparoActual.transform.SetParent(transform, true);
     }
     /// <summary>
-    /// Se llamara para crear un nuevo punto para la linea
+    /// Se llamara para crear un nuevo punto para la linea de disparo
     /// </summary>
     private void AddPunto()
     {
@@ -74,9 +117,11 @@ public class ArmaRayo : Objeto
         }
         //Debug.Log(punto.ToString());
         puntosDelTrail.Add(new Vector3(punto.x + aux, punto.y, 0));
+        numUsos--;
+
     }
     /// <summary>
-    /// Se llamara para ir uniendo los puntos hasta llegar al final hasta que se no haya sufiencente spuntos o no exista el el rastro actual
+    /// Se llamara para ir uniendo los puntos hasta llegar al final hasta que se no haya sufiencentes puntos o no exista el el rastro actual
     /// </summary>
     private void UpdatePuntosTrail()
     {
@@ -91,7 +136,7 @@ public class ArmaRayo : Objeto
         }
     }
     /// <summary>
-    /// Se llamara cuando queramos generar otra linea de corte
+    /// Se llamara cuando queramos generar otra linea de corte, destruyendo la anterior y generando la nueva
     /// </summary>
     private void DestroyTrailActual()
     {
@@ -103,24 +148,13 @@ public class ArmaRayo : Objeto
         }
     }
     /// <summary>
-    /// Se llamara para ir limpiando el reastro poco a poco
+    /// Se llamara para ir limpiando el reastro a la velocidad determinada de la velocidadDeLimpiezaAsignada y la cantidad de puntos que existan
     /// </summary>
     private void LimpiarPuntosTrail()
     {
-        float velLimpieza = limpiezaSpeed;
+        float velLimpieza = velocidadDeLimpiezaAsignada;
         //NOTE: Esto evita que podamos tener una linea demasiado larga, aumentado su velocidad de limpieza dependiendo de la longuitud de la linea
-        if (puntosDelTrail.Count > 5)
-        {
-            velLimpieza *= puntosDelTrail.Count / 2;
-        }
-        else if (puntosDelTrail.Count > 10)
-        {
-            velLimpieza *= puntosDelTrail.Count / 1;
-        }
-        else
-        {
-            velLimpieza = limpiezaSpeed;
-        }
+        velLimpieza *= puntosDelTrail.Count / 2;
         float distanciaLimpieza = velLimpieza;
 
 
