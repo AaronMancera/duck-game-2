80ae0b016b70421162ebf6011a83708e1151b661
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2023-11-22 21:10:10 +0100

    Script de TP

---
+++ DuckGame2/Assets/Scripts/ControlJugador/TPJugador.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPJugador : MonoBehaviour
+{
+    [Header("Jugador")]
+    [SerializeField] GameObject jugador;
+
+    #region LIMITES PANTALLA
+    private float limiteEjeX = 12;
+    private float limiteEjeY = 8;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posicionX = jugador.transform.position.x;
+        float posicionY = jugador.transform.position.y;
+
+        if (posicionX > limiteEjeX) // Pasa por derecha del todo
+        {
+            jugador.transform.position = new Vector2(-limiteEjeX, posicionY);
+        }
+        
+        if (posicionX < -limiteEjeX) // Pasa por izquierda del todo
+        {
+            jugador.transform.position = new Vector2(limiteEjeX, posicionY);
+
+        }
+        
+        if(posicionY > limiteEjeY) // Pasa por arriba del todo
+        {
+            jugador.transform.position = new Vector2(posicionX, -limiteEjeY);
+
+        }
+        
+        if(posicionY < -limiteEjeY) // Cae por abajo del todo
+        {
+            jugador.transform.position = new Vector2(posicionX, limiteEjeY);
+        }
+    }
+}
