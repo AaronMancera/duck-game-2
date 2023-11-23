8355235fe602da437f12df4e03a5c27eaa2b4925
mathias2000269 <107919161+mathias2000269@users.noreply.github.com>
2023-11-23 21:35:35 +0100

    Nueva version de Mover plataformas
    movimiento de plataformas cn limites

--- DuckGame2/Assets/Scripts/ControlEscenario/MovimientoPlataformas.cs
+++ DuckGame2/Assets/Scripts/ControlEscenario/MovimientoPlataformas.cs
@@ -9,7 +9,11 @@ public class MovimientoPlataformas : MonoBehaviour
     private Vector3[] posicionesIniciales;
     private int plataformaActual;
     private bool DetectarVueltaAPosicion;
+    public float limitadorMovimiento = 10f;
+    private string[] direccionPlataforma = {"izquierda","derecha","arriba","abajo" };
+    private string direccionElegida;
     //private bool plataformaEnMovimiento;
+    private bool direccionContraria;
 
     void Start()
     {
@@ -24,51 +28,130 @@ public class MovimientoPlataformas : MonoBehaviour
         DetectarVueltaAPosicion = false;
         // Cambiar a otra plataforma al azar
         plataformaActual = Random.Range(0, plataformas.Length);
+        // Elegir una dirección aleatoria del array
+        direccionElegida = direccionPlataforma[Random.Range(0, direccionPlataforma.Length)];
     }
 
     void Update()
     {
-        //if (!plataformaEnMovimiento) //Habría que agregar un if aqui para que no llame a la función todo el rato pero no se pq este bool no me funcina
+        //if (!plataformaEnMovimiento) 
         //{
-            //Debug.Log("entra en el if update");
+        //    Debug.Log("entra en el if update");
 
             MoverPlataforma();
         //}
         
     }
 
-    void MoverPlataforma() // *Alomejor es mejor usar un IEnumerator mas adelante* ya que asi podriamos quitar booleanos como detectarvueltaaposicion y agregar una pausa
+    void MoverPlataforma() 
     {
         //Debug.Log("entra en funcion movplat");
+
+        //Debug.Log("La dirección elegida es: " + direccionElegida);
         // Obtener la posición actual de la plataforma seleccionada
         Vector3 posicionActual = plataformas[plataformaActual].position;
-        
-        // Mover la plataforma hacia arriba
-        posicionActual.y += velocidadMovimiento * Time.deltaTime;
-        plataformas[plataformaActual].position = posicionActual;
-        //plataformaEnMovimiento = true;
+
+        // Utilizar un switch case para mover la plataforma según la dirección elegida
+
+            //Debug.Log("switch");
+            switch (direccionElegida)
+            {
+                case "izquierda":
+                    // Mover la plataforma hacia izquierda
+                    posicionActual.x -= velocidadMovimiento * Time.deltaTime;
+                    break;
+
+                case "derecha":
+                    // Mover la plataforma hacia derecha
+                    posicionActual.x += velocidadMovimiento * Time.deltaTime;
+                    break;
+
+                case "arriba":
+                    // Mover la plataforma hacia arriba
+                    posicionActual.y += velocidadMovimiento * Time.deltaTime;
+                    break;
+
+                case "abajo":
+                    // Mover la plataforma hacia abajo
+                    posicionActual.y -= velocidadMovimiento * Time.deltaTime;
+                    
+                    break;
+
+                default:
+                    Debug.LogError("Dirección no reconocida");
+                    break;
+            }
+
+        //// Si la plataforma alcanza los límites superiores o inferiores // proxima version restar al limitador lo recrrido por la plataforma 
+        //if (posicionActual.y > 5.5f)
+        //{
+        //    posicionActual.y = -5.5f;
+        //}
+        //else if (posicionActual.y < -5.5f)
+        //{
+        //    posicionActual.y = 5.5f;
+        //}
+
+        //// Si la plataforma alcanza los límites izquierdos o derechos
+        //if (posicionActual.x > 11.7f)
+        //{
+        //    posicionActual.x = -11.7f;
+        //}
+        //else if (posicionActual.x < -11.7f)
+        //{
+        //    posicionActual.x = 11.7f;
+        //}
+
+        //float limitadorMovimiento2 = limitadorMovimiento;
+        //limitadorMovimiento2 -= Mathf.Abs(velocidadMovimiento * Time.deltaTime);
+
+        //Debug.Log("limitadr : " + limitadorMovimiento2 + "posicion: " + posicionActual + "posicion2: "+ plataformas[plataformaActual].position);
         // Si la plataforma alcanza la posición yDestino
-        if (plataformas[plataformaActual].position.y > 5.5f)
-        {
-            //Debug.Log("entra en el pasar pa abajo");
-
-            // Reposicionar la plataforma por debajo
-            posicionActual.y = -5.5f;
-            plataformas[plataformaActual].position = posicionActual;
-            DetectarVueltaAPosicion = true;
+        if (direccionElegida == "arriba" && posicionActual.y > posicionesIniciales[plataformaActual].y + limitadorMovimiento || direccionElegida == "abajo" && posicionActual.y < posicionesIniciales[plataformaActual].y -limitadorMovimiento)
+            {
+                //Debug.Log("cambio dir");
+                // Invertir la dirección de la plataforma según la dirección actual
+                if (direccionElegida == "arriba")
+                {
+                direccionElegida = "abajo";     
+                }
+                else if (direccionElegida == "abajo")
+                {
+                direccionElegida = "arriba";
+                }
+                DetectarVueltaAPosicion = true;
         }
-        
-        // Si la plataforma ha vuelto a su posición inicial No he podido hacerlo mas corto el mathf approximately no me ha funcinado y si lo comparo con su posicion inicial (==) tampoco
-        if (DetectarVueltaAPosicion && plataformas[plataformaActual].position.y >= posicionesIniciales[plataformaActual].y - 0.08f && plataformas[plataformaActual].position.y <= posicionesIniciales[plataformaActual].y + 0.08f )
-        {
-            //Debug.Log("entra en el detener plat");
+        else if (direccionElegida == "derecha" && posicionActual.x > posicionesIniciales[plataformaActual].x + limitadorMovimiento || direccionElegida == "izquierda" && posicionActual.x < posicionesIniciales[plataformaActual].x - limitadorMovimiento)
+            {
+                //Debug.Log("cambio dir");
+                if (direccionElegida == "izquierda")
+                {
+                direccionElegida = "derecha";
+                }
+                else if (direccionElegida == "derecha")
+                {
+                direccionElegida = "izquierda";
+                }
+                DetectarVueltaAPosicion = true;
+            }
+        plataformas[plataformaActual].position = posicionActual;
 
-            // Detener el movimiento estableciendo la posición a la inicial
-            plataformas[plataformaActual].position = posicionesIniciales[plataformaActual];
-            // Cambiar a otra plataforma al azar
-            plataformaActual = Random.Range(0, plataformas.Length);
-            DetectarVueltaAPosicion = false;
-            //plataformaEnMovimiento = false;
+        // Si la plataforma ha vuelto a su posición inicial No he podido hacerlo mas corto el mathf approximately no me ha funcinado y si lo comparo con su posicion inicial (==) tampoco
+        if ((direccionElegida == "arriba" && posicionActual.y > posicionesIniciales[plataformaActual].y - 0.08f && posicionActual.y < posicionesIniciales[plataformaActual].y + 0.08f) && DetectarVueltaAPosicion || 
+           (direccionElegida == "derecha" && posicionActual.x > posicionesIniciales[plataformaActual].x - 0.08f && posicionActual.x < posicionesIniciales[plataformaActual].x + 0.08f) && DetectarVueltaAPosicion ||
+           (direccionElegida == "izquierda" && posicionActual.x > posicionesIniciales[plataformaActual].x - 0.08f && posicionActual.x < posicionesIniciales[plataformaActual].x + 0.08f) && DetectarVueltaAPosicion ||
+           (direccionElegida == "abajo" && posicionActual.y > posicionesIniciales[plataformaActual].y - 0.08f && posicionActual.y < posicionesIniciales[plataformaActual].y + 0.08f) && DetectarVueltaAPosicion)
+            {
+                //Debug.Log("entra en el detener plat");
+
+                // Detener el movimiento estableciendo la posición a la inicial
+                plataformas[plataformaActual].position = posicionesIniciales[plataformaActual];
+                // Cambiar a otra plataforma al azar
+                plataformaActual = Random.Range(0, plataformas.Length);
+                // Elegir una dirección aleatoria del array
+                direccionElegida = direccionPlataforma[Random.Range(0, direccionPlataforma.Length)];
+                DetectarVueltaAPosicion = false;
+                //plataformaEnMovimiento = false;
         }
     }
 }
