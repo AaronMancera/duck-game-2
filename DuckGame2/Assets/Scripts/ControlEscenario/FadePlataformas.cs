9b3b89f0729a2c4e4fd94d1b8855665ede545502
Escuare <rafaescuare@gmail.com>
2023-11-27 20:08:38 +0100

    Plataformas Fade
    Animator y Script que hace que las plataformas aparezcan y desaparezcan cada x segundos.

---
+++ DuckGame2/Assets/Scripts/ControlEscenario/FadePlataformas.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class FadePlataformas : MonoBehaviour
+{
+    [SerializeField] private float segundosDesactivar, segundosActivar;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(temporizadorDesactivar());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator temporizadorDesactivar()
+    {
+        anim.SetTrigger("Desactivar");
+        yield return new WaitForSeconds(segundosActivar);
+        StartCoroutine(temporizadorActivar());
+    }
+
+    IEnumerator temporizadorActivar()
+    {
+        anim.SetTrigger("Activar");
+        yield return new WaitForSeconds(segundosDesactivar);
+        StartCoroutine(temporizadorDesactivar());
+    }
+}
