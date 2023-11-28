d5f074947e6414cc57ec43c4f6873055ccb4fde9
Aarón <aaronmanceracantador2003@gmail.com>
2023-11-28 18:26:37 +0100

    Animacin pollo

---
+++ DuckGame2/Assets/Scripts/Objetos/Proyectiles/Señuelo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Señuelo : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private bool dañado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void EstadoDañado()
+    {
+
+    }
+}
