5e31d47c293b11e071a1ce6ea74afb12d4fc4295
Aarón <aaronmanceracantador2003@gmail.com>
2023-11-30 19:39:49 +0100

    Nivel2 terminado

--- DuckGame2/Assets/Scripts/ControlEscenario/PlataformasMovimientoManager.cs
+++ DuckGame2/Assets/Scripts/ControlEscenario/PlataformasMovimientoManager.cs
@@ -8,6 +8,12 @@ public class PlataformasMovimientoManager : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector2.up * direccion * velocidad * Time.deltaTime);
+        //transform.Translate(Vector2.up * direccion * velocidad * Time.deltaTime);
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector2 nuevaPoscion = new Vector2(transform.position.x, transform.position.y + direccion);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f,direccion*velocidad);
     }
 }
